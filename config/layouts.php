467b97d32a1871988267bf71d47401a962d1d5d6
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-07-18 18:44:32 +0000

    initial commit

---
+++ config/layouts.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Global Layouts Config.
+ *
+ * Configuration for the theme's global layouts.
+ *
+ * @package   Prismatic
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/prismatic
+ */
+
+ return [
+
+	'full' => [
+		'label' => __( 'Full', 'prismatic' )
+	],
+	'wide' => [
+		'label' => __( 'Wide', 'prismatic' )
+	]
+ ];
