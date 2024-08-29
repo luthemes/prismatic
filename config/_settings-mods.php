60d46da0d516d681cfa4ec13833436a1fdb7864d
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-08-29 05:00:35 +0000

    update text-domain

--- config/_settings-mods.php
+++ config/_settings-mods.php
@@ -50,12 +50,12 @@ use function Backdrop\Theme\is_classicpress;
 	// Default footer credit text.
 	'theme_footer_custom_credit' => function() {
 		$year = gmdate('Y'); // Get the current year
-		$copyright = sprintf(__("&#169; %1\$s. %2\$s.", 'amicable'), $year, Backdrop\Theme\Site\render_home_link());
+		$copyright = sprintf(__("&#169; %1\$s. %2\$s.", 'prismatic'), $year, Backdrop\Theme\Site\render_home_link());
 
 		if (is_classicpress()) {
-			$footer_text = sprintf(__("Powered by %1\$s and %2\$s.", 'amicable'), Backdrop\Theme\Site\render_cp_link(), Backdrop\Theme\Site\render_theme_link());
+			$footer_text = sprintf(__("Powered by %1\$s and %2\$s.", 'prismatic'), Backdrop\Theme\Site\render_cp_link(), Backdrop\Theme\Site\render_theme_link());
 		} else {
-			$footer_text = sprintf(__("Powered by %1\$s and %2\$s.", 'amicable'), Backdrop\Theme\Site\render_wp_link(), Backdrop\Theme\Site\render_theme_link());
+			$footer_text = sprintf(__("Powered by %1\$s and %2\$s.", 'prismatic'), Backdrop\Theme\Site\render_wp_link(), Backdrop\Theme\Site\render_theme_link());
 		}
 
 		return $copyright . ' <br /> ' . $footer_text;
