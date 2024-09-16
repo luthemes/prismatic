72604dd9bc2102e622cfb2ae283185e80cd91b30
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-16 14:45:48 +0000

    make sure we add the correct naming fo get_theme_mod();

--- public/views/content/single.php
+++ public/views/content/single.php
@@ -1,5 +1,5 @@
 <section id="content" class="site-content clear">
-	<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'left-sidebar' ) ); ?>">
+	<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'theme_content_global_layout', 'left-sidebar' ) ); ?>">
 		<main id="main" class="content-area">
 			<?php if ( have_posts() ) : ?>
 				<?php while( have_posts() ) : the_post(); ?>
