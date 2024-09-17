886d29d2ab941a1e2ed91a2edc974fb910e19fda
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-17 15:05:27 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   Prismatic
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/prismatic
+ */
+
+ return [
+
+	'prismatic-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'amicable' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'prismatic-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'amicable' ),
+		'width'  => 896,
+		'height' => 504
+	],
+	'prismatic-landscape-extra-large' => [
+		'label'  => __( 'Landscape: Extra Large', 'amicable' ),
+		'width'  => 1366,
+		'height' => 768
+	],
+];
