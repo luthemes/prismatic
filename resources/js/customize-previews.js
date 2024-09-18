4b84613f606db2b1ebad0de697cb537a2af8e391
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-18 05:17:22 +0000

    use DRY

--- resources/js/customize-previews.js
+++ resources/js/customize-previews.js
@@ -1,27 +1,17 @@
 ( function( $ ) {
 
-    // Update the background color in real-time...
-    wp.customize( 'theme_header_background_color', function( value ) {
-        value.bind( function( newval ) {
-            // Update the CSS for the site footer
-            $('.site-header').css('background-color', newval);
+    // Function to update CSS for any selector and property
+    function updateCSS( control, selector, property ) {
+        wp.customize( control, function( value ) {
+            value.bind( function( newval ) {
+                $( selector ).css( property, newval );
+            });
         });
-    });
+    }
 
-    // Update the background color in real-time...
-    wp.customize( 'theme_footer_background_color', function( value ) {
-        value.bind( function( newval ) {
-            // Update the CSS for the site footer
-            $('.site-footer').css('background-color', newval);
-        });
-    });
-
-    // Update the background color in real-time...
-    wp.customize( 'header_textcolor', function( value ) {
-        value.bind( function( newval ) {
-            // Update the CSS for the site footer
-            $('.site-title-link').css('color', newval);
-        });
-    });
+    // Update background colors in real-time
+    updateCSS( 'theme_header_background_color', '.site-header', 'background-color' );
+    updateCSS( 'theme_footer_background_color', '.site-footer', 'background-color' );
+    updateCSS( 'header_textcolor', '.site-title-link', 'color' );
 
 } )( jQuery );
